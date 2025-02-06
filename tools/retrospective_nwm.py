8035a9ea0e667011547e12670b6e5c2f1c35da51
Tadd.Bindas <Tadd.N.Bindas@rtx.com>
2025-02-06 16:52:05 -0500

    add updates and huc8 work

--- tools/retrospective_nwm.py
+++ tools/retrospective_nwm.py
@@ -67,23 +67,31 @@ def gen_retro_fim(feature_id_file: np.ndarray, date_range: Tuple[str, str], outp
 
 
 if __name__ == "__main__":
-    gen_retro_fim(
-        feature_id_file="/home/tadd.bindas/github/pi4/deliveries/rise_redelivery/near_real-time_sfincs/data/NWM/coffeyville_feature_ids.npy", 
-        date_range=["2019-05-20", "2019-06-03"],
-        output_file="/home/tadd.bindas/github/pi4/deliveries/rise_redelivery/near_real-time_sfincs/data/NWM/retro"
-    )
-    # parser = argparse.ArgumentParser(description="Generate retrospective FIM")
+
+    parser = argparse.ArgumentParser(description="Generate retrospective FIM")
     # parser.add_argument("--fim-version", required=True, help="FIM version")
     # parser.add_argument("--huc", required=False, help="HUC code")
-    # parser.add_argument("--feature_id_file", required=True, help="feature_ids")
-    # parser.add_argument("--date-range", required=True, nargs=2, help="Date range (start end)")
     # parser.add_argument("--hydrofabric-dir", required=False, default = OUTPUTS_DIR, help="Path to hydrofabric directory")
     # parser.add_argument("--huc-shapes", required=False, default = os.path.join('/data','wbd','WBD_National.gpkg'), help="Path to huc gpkg")
     # parser.add_argument("--flow-feat", required=False, default = os.path.join('/data','nwm_hydrofabric',"nwm_flows.gpkg"), help="Path to flow features gpkg")
     # parser.add_argument("--output-timeseries", required=False, default = False, help="Path to flow features gpkg")
 
     # Load NWM flows and HUC8 geodataframes
+    # parser.add_argument("--feature_id_file", required=True, help="feature_ids")
+    # parser.add_argument("--date-range", required=True, nargs=2, help="Date range (start end)")
+    # parser.add_argument("--output_path", required=True, help="Path to the output directory")
     # args = parser.parse_args()
+
+    gen_retro_fim(
+        feature_id_file="/home/tadd.bindas/github/pi4/deliveries/rise_redelivery/near_real-time_sfincs/data/NWM/coffeyville_feature_ids_huc_8.npy", 
+        date_range=["2019-05-20", "2019-06-03"],
+        output_file="/home/tadd.bindas/github/pi4/deliveries/rise_redelivery/near_real-time_sfincs/data/NWM/huc_8_retro"
+    )
+    # gen_retro_fim(
+    #     feature_id_file=args.feature_id_file, 
+    #     date_range=args.date_range,
+    #     output_file=args.output_file
+    # )
     # huc8_gdf = gpd.read_file(args.huc_shapes, layer="WBDHU8")
     # nwm_flows = gpd.read_file(args.flow_feat)
 
